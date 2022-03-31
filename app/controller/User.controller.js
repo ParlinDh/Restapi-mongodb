86f6cdaae217cb4a4b972106f50683fd34b26c7f
Parlin Dh <dhparlin@gmail.com>
2022-04-01 02:37:16 +0700

    menambahkan script di file User.controller.js

--- app/controller/User.controller.js
+++ app/controller/User.controller.js
@@ -1,2 +1,13 @@
 const db = require('../models');
-const User = 
+const User = db.User;
+
+exports.findAll = (req, res) => {
+    User.find()
+        .then((result) => {
+            res.send(result)
+        }).catch((err) => {
+            res.status(500).send({
+                message: err.messagem || "Some error while post data"
+            })
+        });
+}
