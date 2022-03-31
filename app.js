a8bf4eb6b2636a8e3d66ce69a0f12d2cd4db5b78
Parlin Dh <dhparlin@gmail.com>
2022-04-01 02:01:34 +0700

    Mengedit koneksi databse di  file app.js

--- app.js
+++ app.js
@@ -6,10 +6,23 @@ const app = express();
 app.use(express.json())
 app.use(express.urlencoded({ extend: true }))
 
+const db = require('./app/models')
+db.mongoose
+    .connect(db.url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        console.log(`Database Connected`)
+    }).catch((err) => {
+        console.log(`Cannot connect to database`, err);
+        process.exit()
+    });
+
 app.get('/', (req, res) => {
     res.json({
-         message: "Hello world"
-     });
+        message: "Hello world"
+    });
 })
 
 const PORT = 8000
