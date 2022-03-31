6831285869a50dcc07ce081ab2d342b931075b01
Parlin Dh <dhparlin@gmail.com>
2022-04-01 01:37:48 +0700

    menambahkan folder config dan models

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ const app = express();
 app.use(express.json())
 app.use(express.urlencoded({ extend: true }))
 
+app.get('/', (req, res) => {
+    res.json({
+         message: "Hello world"
+     });
+})
+
 const PORT = 8000
 app.listen(PORT, () => {
     console.log(`Server started on http://localhost:${PORT}`);
